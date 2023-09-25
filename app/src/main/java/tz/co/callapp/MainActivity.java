2f6eeb14947c8cead40c84898c40ce82f36f139c
silvar1122 <elizasenkoro@gmail.com>
2023-09-25 23:01:02 +0300

    start

---
+++ app/src/main/java/tz/co/callapp/MainActivity.java
@@ -0,0 +1,20 @@
+package tz.co.callapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        1187119285
+
+//        fdaaba8d7519b0edc7f903aa58e58e373b5cff558b1374dae309a1625582d51f
+
+
+    }
+}
